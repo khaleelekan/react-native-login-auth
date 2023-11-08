fff4912376835cc94ac55ba680831501d3899083
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-11-08 11:48:50 +0100

    new

--- util/auth.js
+++ util/auth.js
@@ -1,9 +1,11 @@
 const server = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBQpOkX0HwpEoJPNroFuPRDn-w_6Ru64_c'
+import axios from 'axios';
+
+const API = 'AIzaSyBQpOkX0HwpEoJPNroFuPRDn-w_6Ru64_c'
 
  function createUser (){
-    axios.post(server,
-        {
-            email: email,
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+ API
+           ,{ email: email,
             password: password,
             returnSecureToken: true
         })
