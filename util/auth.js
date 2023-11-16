158e0cda7e2e6b77ac0afda33b66418e9e10edf4
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-11-16 11:59:37 +0100

    new

--- util/auth.js
+++ util/auth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API = 'AIzaSyBQpOkX0HwpEoJPNroFuPRDn-w_6Ru64_c'
 
- asyn function createUser (email, password){
+ async function createUser (email, password){
     const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+ API
            ,{
             email: email,
