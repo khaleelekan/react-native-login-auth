9f3d8825a032877ca3f1267223fa0f30f10aa300
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-11-16 11:58:49 +0100

    new

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,17 +2,19 @@ import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import createUser from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay'
+import {createUser} from '../util/auth';
+
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating]=useState(false)
 
   async function signUpHandler({email, password})
   {
-    setIsAuthenticating(true)
+    setIsAuthenticating(true);
     await createUser(email, password);
-    setIsAuthenticating(false)
+    setIsAuthenticating(false);
   }
-
-if (isAuthenticating){
+ 
+if (isAuthenticating){ 
   return <LoadingOverlay message='creating user'/>
 }
   return <AuthContent onAuthenticate={signUpHandler}/>;
--- util/auth.js
+++ util/auth.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 const API = 'AIzaSyBQpOkX0HwpEoJPNroFuPRDn-w_6Ru64_c'
 
- function createUser (){
-    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+ API
-           ,{ email: email,
+ asyn function createUser (email, password){
+    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+ API
+           ,{
+            email: email,
             password: password,
             returnSecureToken: true
         })
