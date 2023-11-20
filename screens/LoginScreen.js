ab96be549515d0111f70d45045dcf548a6db5bd9
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-11-20 17:23:39 +0100

    new

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,23 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import  login  from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
-  async function signUpHandler({email, password})
+  async function loginHandler({email, password})
   {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    await login(email, password);
     setIsAuthenticating(false);
   }
  
 if (isAuthenticating){ 
-  return <LoadingOverlay message='creating user'/>
-}else{
-  return <AuthContent isLogin />;
+  return <LoadingOverlay message='login in ...'/>
 }
+  return <AuthContent isLogin onAuthenticate={loginHandler}/>;
 }
+
 
 export default LoginScreen;
